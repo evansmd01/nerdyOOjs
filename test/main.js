d57c6ccd95401a7fbab45a080bed67ea34947faf
Michael Evans <mevans@nerdery.com>
2014-01-03 10:03:30 -0600

    initial commit

---
+++ test/main.js
@@ -0,0 +1,67 @@
+
+require(["jso"], function(jsObjects) {
+
+    /************************************
+    *              BASE
+    ************************************/
+    var LoggerBase = jsObjects.declare({
+        construct: function(color) {
+            this.color = color;
+            this.console = document.getElementById('Console');
+            this.log("In LoggerBase.construct!!!");
+        },
+
+        log: function(message) {
+            var p = document.createElement('p');
+            p.style.color = this.color;
+            p.innerHTML = message;
+            this.console.appendChild(p);
+        }
+    });
+
+    /************************************
+    *        Single Inheritance
+    ************************************/
+    var ClassA = jsObjects.declare({
+        construct: function(color) {
+            this.base.construct(color);
+            this.log("In ClassA.construct");
+        },
+
+        method1: function() {
+            this.log("In ClassA.method1");
+        }
+    }, [LoggerBase]);
+    new ClassA('blue').method1();
+
+    /************************************
+    *  Multiple Levels of Inheritance
+    ************************************/
+
+    var ClassB = jsObjects.declare({
+        construct: function(color) {
+            this.base.construct(color);
+            this.log("In ClassB.construct");
+        },
+
+        method1: function() {
+            this.log("In ClassB.method1");
+        }
+    }, [ClassA]);
+    new ClassB('red').method1();
+
+
+    /************************************
+    *         Deadly Diamond of Death
+    ************************************/
+    var ClassC = jsObjects.declare({
+        construct: function(color) {
+            this.base.construct(color); //calls both parents contruct methods
+            this.log("In ClassC.construct");
+        }
+    }, [ClassA, ClassB]);
+    var c = new ClassC("green");
+    c.method1(); //only calls ClassB's method1. Last one in to the inhertance array wins
+    c.base.method1(); //calls both parents' method1. 
+
+});
